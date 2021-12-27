6e8e6f670b536071fb9d5809f7e3d30b1251aab7
Eslicdm <eslicdm1@gmail.com>
2021-12-27 19:16:29 -0300

    swipe to delete

--- app/src/main/java/com/eslirodrigues/productivetime/ui/screen/TaskScreen.kt
+++ app/src/main/java/com/eslirodrigues/productivetime/ui/screen/TaskScreen.kt
@@ -1,17 +1,21 @@
 package com.eslirodrigues.productivetime.ui.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.eslirodrigues.productivetime.R
@@ -19,6 +23,7 @@ import com.eslirodrigues.productivetime.core.ScreenNav
 import com.eslirodrigues.productivetime.core.TaskState
 import com.eslirodrigues.productivetime.ui.viewmodel.TaskViewModel
 
+@ExperimentalMaterialApi
 @Composable
 fun TaskScreen(
     navController: NavController,
@@ -42,7 +47,42 @@ fun TaskScreen(
             is TaskState.Success -> {
                 LazyColumn {
                     items(result.data) { task ->
-                        TaskItemScreen(task = task)
+                        val state = rememberDismissState(
+                            confirmStateChange = {
+                                if (it == DismissValue.DismissedToStart) {
+                                    viewModel.deleteTaskById(task.id)
+                                }
+                                true
+                            }
+                        )
+                        SwipeToDismiss(
+                            state = state,
+                            background = {
+                                val color = when (state.dismissDirection) {
+                                    DismissDirection.StartToEnd -> Color.Transparent
+                                    DismissDirection.EndToStart -> Color.Red
+                                    null -> Color.Transparent
+                                }
+
+                                Box(
+                                    modifier = Modifier
+                                        .fillMaxSize()
+                                        .background(color = color)
+                                        .padding(8.dp)
+                                ) {
+                                    Icon(
+                                        imageVector = Icons.Default.Delete,
+                                        contentDescription = "Delete",
+                                        tint = Color.White,
+                                        modifier = Modifier.align(Alignment.CenterEnd)
+                                    )
+                                }
+                            },
+                            dismissContent = {
+                                TaskItemScreen(task = task)
+                            },
+                            directions = setOf(DismissDirection.EndToStart)
+                        )
                     }
                 }
             }
