89b51d9a7f9506390d4839be60f795a305be7ece
Eslicdm <eslicdm1@gmail.com>
2022-01-18 19:40:38 -0300

    Making TaskTimerScreen layout

--- app/src/main/java/com/eslirodrigues/productivetime/ui/screen/TaskTimerScreen.kt
+++ app/src/main/java/com/eslirodrigues/productivetime/ui/screen/TaskTimerScreen.kt
@@ -2,15 +2,11 @@ package com.eslirodrigues.productivetime.ui.screen
 
 import android.os.CountDownTimer
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.PauseCircleOutline
 import androidx.compose.material.icons.filled.PlayCircleOutline
 import androidx.compose.material.icons.filled.Replay
@@ -18,13 +14,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.eslirodrigues.productivetime.R
 import com.eslirodrigues.productivetime.core.TimeFormatExt.timeFormat
 import com.eslirodrigues.productivetime.data.datasource.Task
-import com.eslirodrigues.productivetime.ui.screen.destinations.TaskScreenDestination
+import com.eslirodrigues.productivetime.ui.theme.LightOrange
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import java.util.concurrent.TimeUnit
@@ -46,7 +45,7 @@ fun TaskTimerScreen(
 ) {
     BackHandler {
         resetTimer()
-        navigator.navigate(TaskScreenDestination())
+        navigator.popBackStack()
     }
 
     isPlaying = remember { mutableStateOf(false) }
@@ -59,18 +58,41 @@ fun TaskTimerScreen(
 
     textTimer = remember { mutableStateOf(timeLeft!!.value.timeFormat()) }
 
-    Column(modifier = Modifier.fillMaxWidth()) {
+    Column(
+        modifier = Modifier
+            .background(LightOrange)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TopAppBar(
+            title = {
+                Text(stringResource(id = R.string.task_timer), color = Color.White)
+            },
+            backgroundColor = LightOrange,
+            navigationIcon = {
+                IconButton(
+                    onClick = {
+                        navigator.popBackStack()
+                    }
+                ) {
+                    Icon(imageVector = Icons.Default.ArrowBack, stringResource(id = R.string.back), tint = Color.White)
+                }
+            }
+        )
         Text(
             modifier = Modifier.padding(start = 10.dp, top = 13.dp),
-            text = textTimer!!.value
+            text = textTimer!!.value,
+            fontSize = 100.sp,
+            color = Color.White
         )
         Spacer(modifier = Modifier.padding(20.dp))
         IconButton(
-            onClick = { resetTimer() }
+            onClick = { resetTimer() },
         ) {
             Icon(
                 Icons.Default.Replay,
-                contentDescription = stringResource(id = R.string.add_task)
+                contentDescription = stringResource(id = R.string.add_task),
+                tint = Color.White,
             )
         }
         Spacer(modifier = Modifier.padding(20.dp))
@@ -78,17 +100,22 @@ fun TaskTimerScreen(
             onClick = {
                 if (isPlaying!!.value) stopTimer() else startTimer()
                 isPlaying!!.value = !isPlaying!!.value
-            }
+            },
+            modifier = Modifier.size(100.dp)
         ) {
             if (isPlaying!!.value) {
                 Icon(
                     imageVector = Icons.Default.PauseCircleOutline,
                     contentDescription = stringResource(id = R.string.add_task),
+                    tint = Color.White,
+                    modifier = Modifier.size(100.dp)
                 )
             } else {
                 Icon(
                     imageVector = Icons.Default.PlayCircleOutline,
                     contentDescription = stringResource(id = R.string.add_task),
+                    tint = Color.White,
+                    modifier = Modifier.size(100.dp)
                 )
             }
         }
@@ -115,7 +142,7 @@ private fun stopTimer() {
 
 private fun resetTimer() {
     countDownTimer?.cancel()
-    isPlaying!!.value = !isPlaying!!.value
+    isPlaying!!.value = false
     timeLeft!!.value = initialTotalTimeInMillis!!
     textTimer!!.value = initialTotalTimeInMillis!!.timeFormat()
 }
